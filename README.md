619e862f021fa4b1df7746e92a247738a434c8d0
SKO85 <sko85gaming@gmail.com>
2020-07-02 17:52:27 +0200

    Initial Project Setup.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Torch-Shared-Library
-Shared Library for Torch Plugins
+A Collaborative Shared Library for Torch Plugins
+
+# Setup
+- Using the Project requires a Symlink for ..\TorchBinaries and ..\GameBinaries to load the referenced game dll files.
+- Edit the SetupSymLinks.bat file and enter the required paths
+- Call the SetupSymLinks.bat or setup SymLinks manually.
---
+++ Utils/ConfigUtils.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using Torch;
+
+namespace Utils
+{
+    public static class ConfigUtils
+    {
+        public static T Load<T>(TorchPluginBase plugin, string fileName) where T : ViewModel, new()
+        {
+            string filePath = Path.Combine(plugin.StoragePath, fileName);
+            T config = new T();
+            if (File.Exists(filePath))
+            {
+                using (StreamReader streamReader = new StreamReader(filePath))
+                {
+                    var xmlSerializer = new XmlSerializer(typeof(T));
+                    config = (T)xmlSerializer.Deserialize(streamReader);
+                }
+            }
+            else
+            {
+                Save<T>(plugin, config, fileName);
+            }
+            return config;
+        }
+
+        public static void Save<T>(TorchPluginBase plugin, T config, string fileName) where T : ViewModel, new()
+        {
+            string filePath = Path.Combine(plugin.StoragePath, fileName);
+            using (StreamWriter streamWriter = new StreamWriter(filePath))
+            {
+                var xmlSerializer = new XmlSerializer(typeof(T));
+                xmlSerializer.Serialize(streamWriter, config);
+            }
+        }
+    }
+}
