9a40dece152a68ce0f502d98d687c6e06dae3910
SKO85 <sko85gaming@gmail.com>
2020-07-03 23:41:28 +0200

    Cleanup namespaces.

--- Constants.cs
+++ Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Torch.SharedLibrary
+﻿namespace Torch.SharedLibrary
 {
     public class Constants
     {
--- Utils/NetworkUtils.cs
+++ Utils/NetworkUtils.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Sandbox.Engine.Multiplayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using VRage.GameServices;
+using VRage.Network;
+using VRage.Serialization;
 
 namespace Torch.SharedLibrary.Utils
 {
